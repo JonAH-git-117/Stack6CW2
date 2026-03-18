315db027b5c1d8ea7f70f0a9cae2e534336919db
JonAH-git-117 <jjchamberlain2002@gmail.com>
2026-03-18 23:58:03 +0000

    Register new models in admin.py

--- teams/admin.py
+++ teams/admin.py
@@ -8,3 +8,10 @@ admin.site.register(Dependency)
 # added to admin file on 11 / 03 / 2026
 admin.site.register(Organisation)
 admin.site.register(Department)
+# added new models to admin on 18 / 03 / 2026
+admin.site.register(ContactChannel)
+admin.site.register(Repository)
+admin.site.register(Project)
+admin.site.register(Message)
+admin.site.register(Meeting)
+admin.site.register(AuditLog)
