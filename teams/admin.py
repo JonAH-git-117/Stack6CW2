bea9a9b8e94066011c5bffaa267bfe39d2bb19cf
Sedra Elawi <sedraelawi@Mac.lan>
2026-04-07 12:07:48 +0100

    Minor updates and final adjustments

--- teams/admin.py
+++ teams/admin.py
@@ -9,10 +9,8 @@ from .models import (
 admin.site.register(Team)
 admin.site.register(Skill)
 admin.site.register(Dependency)
-# added to admin file on 11 / 03 / 2026
 admin.site.register(Organisation)
 admin.site.register(Department)
-# added new models to admin on 18 / 03 / 2026
 admin.site.register(ContactChannel)
 admin.site.register(Repository)
 admin.site.register(Project)
