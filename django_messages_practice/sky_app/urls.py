a6ca99c88bdecee03c6957e77bf6d90ec6bd8173
Hackermunz <137618906+Hackermunz@users.noreply.github.com>
2026-04-24 15:20:25 +0100

    Add root redirect to messages inbox

--- django_messages_practice/sky_app/urls.py
+++ django_messages_practice/sky_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/messages/inbox/')),
     path('admin/', admin.site.urls),
     path('messages/', include('messages_app.urls')),
 ]
