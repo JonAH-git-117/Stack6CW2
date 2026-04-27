ba5505549a0d3ed17fa230a449dad79962423db2
Sedra Elawi <sedraelawi@Sedras-MacBook-Air.local>
2026-04-27 21:36:56 +0100

    updated messages (draft delete + new message fixes)

--- django_messages_practice/messages_app/urls.py
+++ django_messages_practice/messages_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('new/', views.new_message, name='new_message'),
     path('drafts/', views.drafts, name='drafts'),
     path('sent/', views.sent, name='sent'),
+    path('delete/<int:message_id>/', views.delete_message, name='delete_message'),
 ]
--- django_messages_practice/messages_app/views.py
+++ django_messages_practice/messages_app/views.py
@@ -1,17 +1,22 @@
-from django.contrib import messages as django_messages
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Message
 from .forms import MessageForm
 
+from django.shortcuts import get_object_or_404, redirect
+from .models import Message
 
-@login_required
+def delete_message(request, message_id):
+    if request.method == "POST":
+        message = get_object_or_404(Message, id=message_id)
+        message.delete()
+    return redirect('drafts')
+
+
 def inbox(request):
     messages = Message.objects.all()
     return render(request, 'messages_app/inbox.html', {'messages': messages})
 
 
-@login_required
 def new_message(request):
     if request.method == 'POST':
         form = MessageForm(request.POST)
@@ -24,10 +29,6 @@ def new_message(request):
                 message.is_draft = False
 
             message.save()
-            if message.is_draft:
-                django_messages.success(request, "Message saved as a draft.")
-            else:
-                django_messages.success(request, "Message sent successfully.")
             return redirect('inbox')
     else:
         form = MessageForm()
@@ -35,13 +36,11 @@ def new_message(request):
     return render(request, 'messages_app/new_message.html', {'form': form})
 
 
-@login_required
 def drafts(request):
     messages = Message.objects.filter(is_draft=True)
     return render(request, 'messages_app/drafts.html', {'messages': messages})
 
 
-@login_required
 def sent(request):
     messages = Message.objects.filter(is_draft=False)
     return render(request, 'messages_app/sent.html', {'messages': messages})
